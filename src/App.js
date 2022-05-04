3b2493317cc105129991d50f286b64b12654200c
JonLedo <jon.ledo@hotmail.com>
2022-05-04 05:18:45 -0400

    colors can be added to screen using function

--- src/App.js
+++ src/App.js
@@ -6,8 +6,45 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState('false')
-  
-  return <h2>color generator project</h2>
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+      setError(false)
+
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+    
+  }
+
+  return <>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+        className={`${error ? 'error' : null}`}
+        type="text"
+        placeholder='#f15025'
+        value={color} 
+        onChange={(e) => setColor(e.target.value)}
+        />
+        <button className='btn' type='submit'>Submit</button>
+      </form>
+    </section>
+    <section className="colors">
+      {
+        list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+        })  
+      }
+      
+    </section>
+  </>
 }
 
 export default App
